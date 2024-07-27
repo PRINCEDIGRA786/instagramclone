6fab1fdf8d1274b21011bf2ce788a17fd4f49a84
Prince <121872553+PRINCEDIGRA786@users.noreply.github.com>
2024-07-27 10:35:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,26 @@
 const connectToMongo = require('./db');
 connectToMongo();
 
-// Now let's express:
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-var cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const port = 5000;
 
-// Middleware to enable CORS for Express
 app.use(cors({
   origin: 'https://instagramclone-frontend.vercel.app', // replace with your front-end URL
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST']
 }));
-app.use(express.json()); // This is to let the use of req.body...
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("Connected to Vercel Successfully");
 });
 
-// Routes:
 app.use('/insta/auth', require('./routes/auth'));
 app.use('/insta/post', require('./routes/post'));
 app.use('/insta/update', require("./routes/update"));
@@ -30,7 +28,6 @@ app.use('/insta/follow', require('./routes/follow'));
 app.use('/insta/comments', require('./routes/comments'));
 app.use('/insta/messages', require('./routes/messages')); // Add the messages route
 
-// Socket.IO setup with CORS configuration
 const io = socketIo(server, {
   cors: {
     origin: 'https://instagramclone-frontend.vercel.app', // replace with your front-end URL
@@ -60,4 +57,3 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Instagram app backend listening at port: http://localhost:${port}`);
 });
-
